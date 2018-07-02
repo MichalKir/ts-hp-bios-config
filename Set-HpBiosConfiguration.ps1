2a7b2c1977cf60a38cd5012cf18b55cf84d2f927
MichalKir <michalkirejczyk@outlook.com>
2018-07-02 09:10:17 +0200

    added begin, parameters and bios tool path

--- Set-HpBiosConfiguration.ps1
+++ Set-HpBiosConfiguration.ps1
@@ -1,24 +1,52 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $false, HelpMessage = "Specify if you want to configure BIOS from legacy to EFI")]
+    [ValidateNotNullOrEmpty()]
+    [switch]$ConvertToUefi,
+    [Parameter(Mandatory = $false, HelpMessage = "Specify if you want to use debug mode(changes logging path to localfolder)")]
+    [ValidateNotNullOrEmpty()]
+    [switch]$DebugMode
+)
 function Set-HPBios {
     [CmdletBinding()]
     param (
+        ## Provide folder path to where the parameter is stored
         [Parameter(Mandatory = $true, HelpMessage = "Specify folder that contains BIOS-file")]
         [ValidateScript(
             {
                 if (-not ($_ | Test-Path)) {
                     throw "Folder is missing"
-                    exit 1
                 }
                 else {
                     if ((Get-Item -Path $_).Attributes -notmatch 'Directory') {
                         throw "Path provided is not an directory"
-                        exit 1
                     }
                 }
                 return $true
             }
         )]
         [System.IO.FileInfo]
-        $FolderPath
+        $FolderPath,
+        ## Specify what exit codes are considered as success, default: 0, 1, 5, 13, 17 (See HP-documentation for more info)
+        [Parameter(Mandatory = $false, HelpMessage = "Specify list of approved exit codes")]
+        [ValidateNotNullOrEmpty()]
+        [string[]]$ApprovedExitCodes = @(0, 1, 5, 13, 17)
     )
-}
+    begin {
+        Write-Verbose -Message "Creating variables for BIOS-config tool"
 
+        ## Provide path for correct Config Utility based on OS-arch
+        if ((Get-CimInstance -ClassName Win32_OperatingSystem).OSArchitecture -match '64') {
+            $biosTool = Join-Path -Path $PSScriptRoot -ChildPath "Tools\BiosConfigUtility\BiosConfigUtility64.exe"
+        }
+        else {
+            $biosTool = Join-Path -Path $PSScriptRoot -ChildPath "Tools\BiosConfigUtility\BiosConfigUtility.exe"
+        }
+        
+        ## Test if bios tool is present
+        Write-Verbose -Message "Attempting to test that $biosTool is present"
+        if (-not(Test-Path -Path $biosTool)) {
+            throw "Failed to locate BIOS-tool, check if path is correct"
+        }
+    }
+}
